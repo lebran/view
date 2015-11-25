689f93fdfbafa349cb3a5d9dcae624b7f2ed0b54
Roman Kritskyi <itoktor@gmail.com>
2015-11-25 15:16:37 +0200

    feat(view): initial commit

---
+++ src/View.php
@@ -0,0 +1,243 @@
+<?php
+namespace Lebran;
+
+use Lebran\Utils\Storage;
+use Lebran\View\Exception;
+use Lebran\View\Extension\ExtensionInterface;
+
+/**
+ * Lebran\Mvc\View is a native PHP template system that's fast and easy to extend.
+ * It's inspired by compiled template engines (Twig, Blade, ...).
+ *
+ *                              Examples
+ *  <code>
+ *      $view = new Lebran\Mvc\View();
+ *      $view->addExtension(new Lebran\Mvc\View\Blocks())
+ *              ->addFolder('main', '/templates/main')
+ *              ->addFolder('new', '/templates/new')
+ *              ->enableShortTags()
+ *              ->render('new::news');
+ *
+ *      // templates/new/news.php
+ *
+ *      <?php $layout('main::index')?>
+ *
+ *      <?php $block('data')?>
+ *          Some text.
+ *      <?php $endblock()?>
+ *
+ *      // templates/main/index.php
+ *
+ *      <?php $output('data', 'Some default text')?>
+ *
+ *  </code>
+ *
+ * @package    Mvc
+ * @subpackage View
+ * @version    2.0.0
+ * @author     Roman Kritskiy <itoktor@gmail.com>
+ * @license    GNU Licence
+ * @copyright  2014 - 2015 Roman Kritskiy
+ */
+class View extends Storage
+{
+    /**
+     * @var array Storage for extensions.
+     */
+    protected $extensions = [];
+
+    /**
+     * @var array Storage for extension methods.
+     */
+    protected $methods = [];
+
+    /**
+     * @var string The name of last rendered template.
+     */
+    protected $template;
+
+    /**
+     * @var string Paths to the view folders.
+     */
+    protected $folders = [];
+
+    /**
+     * @var array Stack for parent files.
+     */
+    protected $layouts = [];
+
+    /**
+     * @var string Child content.
+     */
+    protected $content = '';
+
+    /**
+     * Initialisation. Prepare extensions.
+     *
+     * @param array $extensions An array of extensions.
+     * @param array $data       An array of data.
+     */
+    public function __construct(array $extensions = [], array $data = [])
+    {
+        parent::__construct($data);
+        foreach ($extensions as $extension) {
+            $this->addExtension($extension);
+        }
+    }
+
+    /**
+     * Adds extensions.
+     *
+     * @param ExtensionInterface $extension Extension object.
+     *
+     * @return object View object.
+     */
+    public function addExtension(ExtensionInterface $extension)
+    {
+        $this->extensions[$extension->getName()] = $extension;
+        foreach ($extension->getMethods() as $key => $value) {
+            $this->methods[$key] = [$extension, $value];
+        }
+        return $this;
+    }
+
+    /**
+     * Adds folder for views.
+     *
+     * @param string $name   The name of folder.
+     * @param string $folder Path to folder.
+     *
+     * @return object View object.
+     */
+    public function addFolder($name, $folder)
+    {
+        $this->folders[$name] = rtrim(trim($folder), '/').'/';
+        return $this;
+    }
+
+    /**
+     * Generates path for template.
+     *
+     * @param string $template The name of template.
+     *
+     * @return string Resolved path.
+     * @throws \Lebran\Mvc\View\Exception
+     */
+    protected function resolvePath($template)
+    {
+        $parts = explode('::', $template);
+        if (count($parts) === 1) {
+            foreach ($this->folders as $folder) {
+                if (is_file($folder.$parts[0].'.php')) {
+                    return $folder.$parts[0].'.php';
+                }
+            }
+        } else if (count($parts) === 2 && is_file($this->folders[$parts[0]].$parts[1].'.php')) {
+            return $this->folders[$parts[0]].$parts[1].'.php';
+        }
+
+        throw new Exception(
+            'The template name "'.$this->template.'" is not valid. '.
+            'Do not use the folder namespace separator "::" more than once.'
+        );
+    }
+
+    /**
+     * Render the template and layout.
+     *
+     * @param string $template The name of template.
+     * @param array  $data     An array of data.
+     *
+     * @return string Rendered template.
+     * @throws \Lebran\Mvc\View\Exception
+     */
+    public function render($template, array $data = [])
+    {
+        $this->template = $template;
+        $this->storage  = array_merge_recursive($this->storage, $data);
+
+        extract($this->storage);
+        ob_start();
+
+        include $this->resolvePath($this->template);
+
+        if (0 === count($this->layouts)) {
+            return ob_get_clean();
+        } else {
+            $this->content = ob_get_clean();
+            return $this->render(array_pop($this->layouts));
+        }
+    }
+
+    /**
+     * Magic method used to call extension functions.
+     *
+     * @param string $method     The name of method.
+     * @param array  $parameters The params of method.
+     *
+     * @return mixed Method response.
+     * @throws \Lebran\Mvc\View\Exception
+     */
+    public function __call($method, $parameters)
+    {
+        if (array_key_exists($method, $this->methods)) {
+            return call_user_func_array($this->methods[$method], $parameters);
+        } else {
+            throw new Exception('The extension method "'.$method.'" not found.');
+        }
+    }
+
+    /**
+     * Magic method used to get extension.
+     *
+     * @param string $name The name of extension.
+     *
+     * @return object Extension object.
+     * @throws \Lebran\Mvc\View\Exception
+     */
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->extensions)) {
+            return $this->extensions[$name];
+        } else {
+            throw new Exception('The extension "'.$name.'" not found.');
+        }
+    }
+
+    /**
+     * Set the template's layout.
+     *
+     * @param string $layout The name of layout
+     *
+     * @return void
+     */
+    protected function layout($layout)
+    {
+        $this->layouts[] = $layout;
+    }
+
+    /**
+     * Prints child content.
+     *
+     * @return void
+     */
+    protected function content()
+    {
+        echo $this->content;
+    }
+
+    /**
+     * Include view.
+     *
+     * @param string $template The name of template.
+     *
+     * @return void
+     * @throws \Lebran\Mvc\View\Exception
+     */
+    protected function import($template)
+    {
+        $this->template = $template;
+        extract($this->storage);
+        include $this->resolvePath($this->template);
+    }
+}
---
+++ src/View/Extension/Assets.php
@@ -0,0 +1,173 @@
+<?php
+namespace Lebran\View\Extension;
+
+/**
+ * This class provides easy way include assets (styles, scripts and images).
+ *
+ * @package    Mvc
+ * @subpackage View
+ * @version    2.0.0
+ * @author     Roman Kritskiy <itoktor@gmail.com>
+ * @license    GNU Licence
+ * @copyright  2014 - 2015 Roman Kritskiy
+ */
+class Assets implements ExtensionInterface
+{
+    /**
+     * @var array Preferred order of attributes.
+     */
+    protected static $attributes = [
+        'action',
+        'method',
+        'type',
+        'id',
+        'name',
+        'value',
+        'href',
+        'src',
+        'width',
+        'height',
+        'cols',
+        'rows',
+        'size',
+        'maxlength',
+        'rel',
+        'media',
+        'accept-charset',
+        'accept',
+        'tabindex',
+        'accesskey',
+        'alt',
+        'title',
+        'class',
+        'style',
+        'selected',
+        'checked',
+        'readonly',
+        'disabled',
+    ];
+
+    /**
+     * @var string  Path to assets folder.
+     */
+    public $folder;
+
+    /**
+     * Gets the name of extension.
+     *
+     * @return string Extension name.
+     */
+    public function getName()
+    {
+        return 'assets';
+    }
+
+    /**
+     * Gets array: alias => method name.
+     *
+     * @return array An array of methods name.
+     */
+    public function getMethods()
+    {
+        return [
+            'css'  => 'style',
+            'js'   => 'script',
+            'img'  => 'image',
+            'attr' => 'attr'
+        ];
+    }
+
+    /**
+     * Initialisation. Sets folder.
+     *
+     * @param string $folder Path to folder.
+     */
+    public function __construct($folder = '')
+    {
+        $this->folder = '/'.trim(trim($folder), '/').'/';
+    }
+
+    /**
+     * Creates a style sheet link element.
+     *
+     * <code>
+     *      <?=$this->style('css/style.css')?>
+     * </code>
+     *
+     * @param string $name The relative path to the file from a folder.
+     *
+     * @return string Style sheet link
+     */
+    public function style($name)
+    {
+        $attr = ['href' => $this->folder.$name, 'rel' => 'stylesheet', 'type' => 'text/css'];
+        return '<link'.$this->attr($attr).'/>'.PHP_EOL;
+    }
+
+    /**
+     * Creates a script link.
+     *
+     *      <?=$this->script('js/validator.js')?>
+     *
+     * @param string $name The relative path to the file from a folder.
+     *
+     * @return string Script link.
+     */
+    public function script($name)
+    {
+        $attr = ['src' => $this->folder.$name, 'type' => 'text/javascript'];
+        return '<script'.$this->attr($attr).'></script>'.PHP_EOL;
+    }
+
+    /**
+     * Creates a image link.
+     *
+     *      <?=$this->img('images/logo.png', array('alt' => 'logo'))?>
+     *
+     * @param string $name The relative path to the file from a folder.
+     * @param array  $attr An array of attributes for image.
+     *
+     * @return string Image link.
+     */
+    public function image($name, $attr = [])
+    {
+        $attr += ['src' => $this->folder.$name];
+        return '<img '.$this->attr($attr).'/>';
+    }
+
+    /**
+     * Compiles an array of HTML attributes into an attribute string.
+     *
+     * @param array $attr An array of parameters.
+     *
+     * @return string Compiled parameters.
+     */
+    public function attr(array $attr = null)
+    {
+        if (empty($attr)) {
+            return '';
+        }
+
+        $sorted = array();
+        foreach (self::$attributes as $key) {
+            if (isset($attr[$key])) {
+                $sorted[$key] = $attr[$key];
+            }
+        }
+        $attr     = $sorted + $attr;
+        $compiled = '';
+        foreach ($attr as $key => $value) {
+            if ($value === null) {
+                continue;
+            }
+            if (is_int($key)) {
+                $key = $value;
+            }
+            $compiled .= ' '.$key;
+            if ($value) {
+                $compiled .= '="'.$value.'"';
+            }
+        }
+        return $compiled;
+    }
+}
